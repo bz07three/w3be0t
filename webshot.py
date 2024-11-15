77970e83b6d745cc0e585f6f13c778c30ee326d4
baizhen <133853117+bz07three@users.noreply.github.com>
2024-11-15 21:15:30 +0800

    Update webshot.py

--- webshot.py
+++ webshot.py
@@ -1,14 +1,14 @@
 from urllib.parse import urlparse
-
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from tqdm import trange
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 print("""
 $$\      $$\           $$\                 $$\                  $$\     
 $$ | $\  $$ |          $$ |                $$ |                 $$ |    
-$$ |$$$\ $$ | $$$$$$\  $$$$$$$\   $$$$$$$\ $$$$$$\   $$$$$$\ $$$$$$\   
+$$ |$$$\ $$ | $$$$$$\  $$$$$$\   $$$$$$\ $$$$$$\   $$$$$$\ $$$$$$\   
 $$ $$ $$\$$ |$$  __$$\ $$  __$$\ $$  _____|$$  __$$\ $$  __$$\\_$$  _|  
 $$$$  _$$$$ |$$$$$$$$ |$$ |  $$ |\$$$$$$\  $$ |  $$ |$$ /  $$ | $$ |    
 $$$  / \$$$ |$$   ____|$$ |  $$ | \____$$\ $$ |  $$ |$$ |  $$ | $$ |$$\ 
@@ -21,10 +21,8 @@ $$  /   \$$ |\$$$$$$$\ $$$$$$$  |$$$$$$$  |$$ |  $$ |\$$$$$$  | \$$$$  |
 opt = webdriver.EdgeOptions()
 opt.add_argument('--headless')
 opt.add_argument('--disable-gpu')
-opt.add_argument('--ignore-certificate-errors')  # 忽略证书错误
-opt.add_argument('--ignore-ssl-errors')  # 忽略SSL错误
-opt.add_argument('--allow-insecure-localhost')  # 允许不安全的本地主机
 opt.add_experimental_option('excludeSwitches', ['enable-logging'])
+
 
 # 初始化
 def init():
@@ -32,6 +30,7 @@ def init():
     service = Service(service=path)
     driver = webdriver.Edge(service=service, options=opt)
     return driver
+
 
 # 格式化网址
 def formatting(file_path):
@@ -43,6 +42,7 @@ def formatting(file_path):
             temp_file.write(line)
     os.replace(temp_file_path, file_path)
 
+
 # 读取网址
 def extract_domain(url):
     try:
@@ -51,6 +51,8 @@ def extract_domain(url):
         return domain
     except ValueError:
         return "Invalid URL"
+
+
 def read():
     pathweb = "web.txt"
     websites = []
@@ -58,6 +60,7 @@ def read():
         for line in f:
             websites.append(line.strip())
     return websites
+
 
 def remove_http_prefix(url):
     if url.startswith("http://"):
@@ -67,12 +70,14 @@ def remove_http_prefix(url):
     else:
         return url
 
+
 # 处理单个网站的截图
-def process_website(driver, website):
+def process_website(website):
+    driver = init()
     try:
         driver.get(website)
         domain = extract_domain(website)
-        driver.implicitly_wait(5)  # 等待页面加载
+        driver.implicitly_wait(2)  # 等待页面加载
         driver.execute_script("document.body.style.zoom='0.5'")
         title = driver.title
         web = remove_http_prefix(website)
@@ -82,21 +87,22 @@ def process_website(driver, website):
             picture_name = str(domain)
         screenshot_path = 'screenshot/{}.png'.format(str(picture_name))
         driver.save_screenshot(screenshot_path)
-
     except Exception as e:
         print(f"处理网站 {website} 时出错：{str(e)}")
+    finally:
+        driver.quit()
+
 
 if __name__ == "__main__":
     formatting("web.txt")
     websites = read()
-    driver = init()
 
     # 确保screenshot文件夹存在
     if not os.path.exists('screenshot'):
         os.makedirs('screenshot')
 
     # 处理网站并发截图
-    for _ in trange(len(websites)):
-        process_website(driver, websites[_])
-
-    driver.quit()
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(process_website, website) for website in websites]
+        for future in as_completed(futures):
+            future.result()  # 可以在这里处理每个任务的结果，如果有的话
